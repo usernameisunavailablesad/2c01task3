922d6ca5bcf08acbc2c60ee6c39ef0012981dcb6
usernameisunavailablesad <115450071+usernameisunavailablesad@users.noreply.github.com>
2022-10-17 14:15:21 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+})
+basic.forever(function () {
+	
+})
